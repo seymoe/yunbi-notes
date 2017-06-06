5f55d6dbe399bbc0fcd8ca97ebfb793a116a5739
IAQV1WK0NG5GMAR\Administrator <txiliang@163.com>
2017-06-06 17:49:18 +0800

    change the layout

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,8 +6,11 @@ const ListItem = ({ id, title, clickPreview, changeStatusShow, showNoteDetail, c
       clickPreview(id)
       changeStatusShow(!showNoteDetail)
     }}>
-      <p className="listtitle">{title}</p>
-      <i className="iconfont">&#xe65f;</i>
+      <div className="listbox">
+        <h3 className="listtitle">{title}</h3>
+        <p>所谓“键盘可访问性”，指的是用户只使用键盘访问网站的能力。例如，我们的iMac鼠标没电了，或者鼠标坏了，或者在智能电视中访问我们的网站，此时就只能依赖于键盘访问了。</p>
+        <span><i className="iconfont">&#xe64d;</i>2017.6.6</span>
+      </div>
     </li>
   )
 }
