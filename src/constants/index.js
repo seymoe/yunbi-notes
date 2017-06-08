53363e3f89d383bab00f807c723c54829637d868
ximolang <ximolang@yeah.net>
2017-06-08 13:20:25 +0800

    try it again

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const ADD_NOTE = 'ADD_NOTE'
-export const DELETE_NOTE = 'DELETE_NOTE'
-export const SHOW_NOTE = 'SHOW_NOTE'
-//export const MODIFY_NOTE = 'MODIFY_NOTE'
-//export const SAVE_NOTE = 'SAVE_NOTE'
-export const SHOW_LAYER = 'SHOW_LAYER'
-export const SHOW_EDITER = 'SHOW_EDITER'
